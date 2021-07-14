b63d0251354981bbd6a9c12a0c793a9d375e6780
priyadharshini-annamalai <61078018+priyadharshini-annamalai@users.noreply.github.com>
2021-07-14 15:24:51 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var countries=["India","USA","Japan","Russia"];
+var states= new Array("TN","Delhi","rajasthan","kerala");
+console.log("states[1]");
+console.log(states.length);
+console.log(states.pop());
+console.log(states.shift());
+
+console.log(states);
+console.log(states.unshift("assam"));
+console.log(states.length);
+console.log(states);
+console.log(states.indexOf("assam"));
